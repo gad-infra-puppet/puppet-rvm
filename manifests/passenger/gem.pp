3d92ab254fc373aa22b44965b2bdcb8362f55e34
Mingxiang.Zhang <ming.zhang@gree.net>
2013-05-16 16:48:21 +0900

    remove require since passing gemset ruby is possible

--- manifests/passenger/gem.pp
+++ manifests/passenger/gem.pp
@@ -2,7 +2,6 @@ class rvm::passenger::gem($ruby_version, $version) {
   rvm_gem {
     "passenger":
       ensure       => $version,
-      require => Rvm_system_ruby["${ruby_version}"],
       ruby_version => $ruby_version;
   }
 }
